370ce7e2540beced1e0bc894001ee92d39d33693
fix <agile.mr.fix@gmail.com>
2018-12-29 14:13:58 +0200

    +

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -19,6 +19,6 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        //
+        "/712285308:AAET4okFG1d1CTu6vENUZJ7ybNUMqECep8M/webhook"
     ];
 }
