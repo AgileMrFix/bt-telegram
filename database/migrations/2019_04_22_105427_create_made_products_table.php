be63c257ccd89a3a9915962fb97524c662ca063c
fix <agile.mr.fix@gmail.com>
2019-04-22 14:34:23 +0300

    new tables

---
+++ database/migrations/2019_04_22_105427_create_made_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMadeProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('made_products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('production_report_id')->index();
+            $table->string('name');
+            $table->string('option');
+            $table->string('option_number');
+            $table->string('amount');
+            $table->string('weight');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('made_products');
+    }
+}
---
+++ database/migrations/2019_04_22_111554_create_production_reports_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductionReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('production_reports', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('doc_number');
+            $table->string('employee');
+            $table->dateTime('opened_at')->nullable();
+            $table->dateTime('closed_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('production_reports');
+    }
+}
